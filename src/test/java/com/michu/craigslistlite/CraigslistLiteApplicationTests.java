202e4a58c9c7714a50fe6f4e355e7e6a5271d80b
apokalipsa44 <m_lachowicz@wp.pl>
2019-10-19 12:04:06 +0200

    initial commit

---
+++ src/test/java/com/michu/craigslistlite/CraigslistLiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michu.craigslistlite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraigslistLiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
